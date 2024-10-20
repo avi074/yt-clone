cf03c33cb19f681e35d29dbe7a366db15a4b113b
Avi074 <gautamavinash.ag@gmail.com>
2024-10-20 17:51:49 +0530

    [Server]: logging middleware updated

---
+++ server/middlewares/logReqRes.js
@@ -0,0 +1,29 @@
+import logger from "../utils/logger.js"
+
+/**
+ * Logs the basic information like request method
+ * with their respective url & the response of the request
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ * @returns
+ */
+export default function logReqRes(req, res, next) {
+  logger.log(`Request at ${new Date().toLocaleString()}`)
+  logger.info(`[REQ] ${req.method} ${req.originalUrl}`)
+  
+  res.on("finish", () => {
+    const msg = `[RES] ${res.statusCode} ${res.statusMessage}`
+    
+    if (res.statusCode < 300) {
+      logger.success(msg)
+    } else if (res.statusCode < 400) {
+      logger.warn(msg)
+    } else {
+      logger.error(msg)
+    }
+    logger.log(`Response at ${new Date().toLocaleString()}`)
+  })
+  next()
+}
